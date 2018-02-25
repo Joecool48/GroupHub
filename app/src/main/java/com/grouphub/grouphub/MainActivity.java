62f313e4522da936e09f89ba69350a48566dd67a
jimmyhaitao <haitaol2@illinois.edu>
2018-02-24 18:02:00 -0600

    finished icons.

--- app/src/main/java/com/grouphub/grouphub/MainActivity.java
+++ app/src/main/java/com/grouphub/grouphub/MainActivity.java
@@ -2,6 +2,13 @@ package com.grouphub.grouphub;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Toast;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.AdapterView.OnItemClickListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +16,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        android.widget.GridView gridview = (android.widget.GridView) findViewById(R.id.gridview);
+        gridview.setAdapter(new ImageAdapter(this));
+
 
     }
 
